febf670c25ddeaae27d2200cb9a7c89203a6d2ef
Ayman <Aymanliban2003@gmail.com>
2023-08-16 00:37:08 +0300

    Second Commit

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,41 +7,53 @@ import {
   FaTwitterSquare,
 } from 'react-icons/fa';
 
-export default function Footer(){
+export default function Footer() {
   return (
     <div className='max-w-[1240px] mx-auto py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-300'>
       <div>
         <h1 className='w-full text-3xl font-bold text-[#00df9a]'>BetaHouse</h1>
         <p className='py-4'>BetaHouse Waa Companyga ugu wata Guri Kireynta</p>
         <div className='flex justify-between md:w-[75%] my-6'>
-            <FaFacebookSquare size={30} />
-            <FaInstagram size={30} />
-            <FaTwitterSquare size={30} />
-            <FaGithubSquare size={30} />
-            <FaDribbbleSquare size={30} />
+          <FaFacebookSquare size={30} />
+          <FaInstagram size={30} />
+          <FaTwitterSquare size={30} />
+          <FaGithubSquare size={30} />
+          <FaDribbbleSquare size={30} />
         </div>
       </div>
-      <div className='lg:col-span-2 flex justify-between mt-6'>
-    <div>
-        <h6 className='font-medium text-gray-400'>Links</h6>
-        <ul>
-            <a href="#" className='decoration-solid decoration-green'><li className='py-2 text-sm'>Home</li></a>
-            <a href="#" className='decoration-solid decoration-green'><li className='py-2 text-sm'>About</li></a>
-            <a href="#" className='decoration-solid decoration-green'><li className='py-2 text-sm'>HouseType</li></a>
-            <a href="#" className='decoration-solid decoration-green'><li className='py-2 text-sm'>Contact</li></a>
-        </ul>
-    </div>
-    <div>
-        <h6 className='font-medium text-gray-400'>Legal</h6>
-        <ul>
-        <a href="#" className='decoration-solid decoration-green'><li className='py-2 text-sm'>Claim</li></a>
-            <a href="#" className='decoration-solid decoration-green'><li className='py-2 text-sm'>Policy</li></a>
-            <a href="#" className='decoration-solid decoration-green'><li className='py-2 text-sm'>Terms</li></a>
-        </ul>
-    </div>
+      <div className='lg:col-span-2 flex justify-center mt-6'>
+        <div>
+          <h6 className='font-medium text-gray-400'>Links</h6>
+          <ul>
+            <li className='py-2 text-sm'>
+              <a href="#" className='decoration-solid decoration-green'>Home</a>
+            </li>
+            <li className='py-2 text-sm'>
+              <a href="#" className='decoration-solid decoration-green'>About</a>
+            </li>
+            <li className='py-2 px-4 text-sm'>
+              <a href="#" className='decoration-solid decoration-green'>HouseType</a>
+            </li>
+            <li className='py-2 text-sm'>
+              <a href="#" className='decoration-solid decoration-green'>Contact</a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h6 className='font-medium text-gray-400'>Legal</h6>
+          <ul>
+            <li className='py-2 text-sm'>
+              <a href="#" className='decoration-solid decoration-green'>Claim</a>
+            </li>
+            <li className='py-2 text-sm'>
+              <a href="#" className='decoration-solid decoration-green'>Policy</a>
+            </li>
+            <li className='py-2 text-sm'>
+              <a href="#" className='decoration-solid decoration-green'>Terms</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
 }
-
-
--- src/Components/Qurxis.jsx
+++ src/Components/Qurxis.jsx
@@ -1,7 +1,48 @@
-import React from 'react';
-import Typed from 'react-typed';
+import React, { useState, useEffect } from 'react';
 
- export default function Qurxis(){
+export default function Qurxis() {
+  const [typedText, setTypedText] = useState('');
+  const textOptions = ['Trusted', 'Reliable', 'AND Ugu Shidan'];
+  const typingSpeed = 100;
+  const backspacingSpeed = 120;
+
+  useEffect(() => {
+    let currentText = '';
+    let currentIndex = 0;
+    let isDeleting = false;
+    let timer;
+
+    const typeText = () => {
+      const currentOption = textOptions[currentIndex];
+      const currentLength = currentText.length;
+
+      if (isDeleting) {
+        currentText = currentOption.substring(0, currentLength - 1);
+      } else {
+        currentText = currentOption.substring(0, currentLength + 1);
+      }
+
+      setTypedText(currentText);
+
+      if (!isDeleting && currentText === currentOption) {
+        isDeleting = true;
+        timer = setTimeout(typeText, backspacingSpeed);
+      } else if (isDeleting && currentText === '') {
+        isDeleting = false;
+        currentIndex = (currentIndex + 1) % textOptions.length;
+        timer = setTimeout(typeText, typingSpeed);
+      } else {
+        timer = setTimeout(typeText, isDeleting ? backspacingSpeed : typingSpeed);
+      }
+    };
+
+    timer = setTimeout(typeText, typingSpeed);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
   return (
     <div className='text-white'>
       <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
@@ -13,15 +54,11 @@ import Typed from 'react-typed';
         </h1>
         <div className='flex justify-center items-center'>
           <p className='md:text-5xl sm:text-4xl text-xl font-bold py-4'>
-            Most 
+            Most
           </p>
-          <Typed
-          className='md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2'
-            strings={['Trusted', 'Reliable', 'AND Ugu Shidan']}
-            typeSpeed={120}
-            backSpeed={140}
-            loop
-          />
+          <p className='md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2'>
+            {typedText}
+          </p>
         </div>
         <p className='md:text-2xl text-xl font-bold text-gray-500'>Kireyso guri laga soo shaqeeyay</p>
         <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black'>Houses & Home</button>
@@ -29,5 +66,3 @@ import Typed from 'react-typed';
     </div>
   );
 }
-
-
