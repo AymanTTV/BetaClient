5dc3c6401ea7f4206c794c96e61dabb998258358
Ayman <Aymanliban2003@gmail.com>
2023-08-20 21:36:38 +0300

    13enth commit

--- src/Pages/HouseType.jsx
+++ src/Pages/HouseType.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "../Components/Navbar";
 import Footer from '../Components/Footer';
 import Banner from "../Components/Banner";
-import HouseList from '../components/HouseList'
+import HouseList from '../components/HouseList.jsx'
 
 export default function HouseType(){
     return(
